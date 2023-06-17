a26a72bc0da4688d77a853698f63a99a477b49e8
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-06-17 19:51:27 +0400

    DecentralizedFileStorage

---
+++ DecentralizedFileStorage.sol
@@ -0,0 +1,8 @@
+# Decentralized File Storage
+
+
+# Like every other application, we need a dedicated file storage system (similar to Amazon S3 for Web2 applications).
+#  Databases essentially are tabular data, while file storage is that folder-based data you need to store your large media files.
+#  IPFS is the most common P2P file system protocol with excellent community support.
+#  Arweave is another option focused on keeping data permanently.
+#  You can use platforms like Filecoin, Skynet, Storj, and 0Chain to host these file storage systems.
